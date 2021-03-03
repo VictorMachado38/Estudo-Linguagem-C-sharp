a57e7aac660ef54403a0f852258cf87d4980e637
VictorMachado38 <54145667+VictorMachado38@users.noreply.github.com>
2021-03-03 14:45:10 -0300

    Implementação
    Final

--- Academia-com-banco-de-dados/Globais.cs
+++ Academia-com-banco-de-dados/Globais.cs
@@ -11,7 +11,8 @@ namespace Academia_com_banco_de_dados
         public static string versao = "1.0";
         public static Boolean logado = false;
         public static int nivel = 0; // 1 = Básico , 2 = Gerente , 3 = Master
-        public static string caminho = System.Environment.CurrentDirectory;
+        //public static string caminho = System.Environment.CurrentDirectory;
+        public static string caminho = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
         public static string nomaBanco="BD_Academia.db";
         public static string caminhoBanco = caminho+@"\banco\";
         public static string caminhoFoto = caminho+@"\fotos\";
